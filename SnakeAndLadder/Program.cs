baca97e43e48564482236a56a3da9bb40bd1d6e4
progwruk <wmeshram@gmail.com>
2022-08-24 15:20:35 +0530

    [Wrukshali] Add: Welcome to snake and ladder game

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder game");
+        }
+    }
+}
+
